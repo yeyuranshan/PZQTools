b76c74706b017d90e1e037c748c6580bbeedf766
潘振权 <panzhenquan@jianke.com>
2017-12-08 17:08:30 +0800

    添加一些常用方法

---
+++ Tools/Tools/PANTools/BaseTools/SingleManager.m
@@ -0,0 +1,75 @@
+//
+//  SingleManager.m
+//  Tools
+//
+//  Created by 潘振权 on 2017/12/8.
+//  Copyright © 2017年 潘振权. All rights reserved.
+//单例类
+// 使用方法：MySingleManagerH(teb)--->sharedTeb
+//.h文件
+#define  MySingleManagerH(name) +(instancetype)shared##name;
+
+//.m文件
+
+#if __has_feature(objc_arc)
+
+#define MySingleManagerM(name)\
+static id _instace; \
+\
++ (id)allocWithZone:(struct _NSZone *)zone \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instace = [super allocWithZone:zone]; \
+}); \
+return _instace; \
+} \
+\
++ (instancetype)shared##name \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instace = [[self alloc] init]; \
+}); \
+return _instace; \
+} \
+\
+- (id)copyWithZone:(NSZone *)zone \
+{ \
+return _instace; \
+}
+
+#else
+#define MySingleManagerM(name) \
+static id _instace; \
+\
++ (id)allocWithZone:(struct _NSZone *)zone \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instace = [super allocWithZone:zone]; \
+}); \
+return _instace; \
+} \
+\
++ (instancetype)shared##name \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instace = [[self alloc] init]; \
+}); \
+return _instace; \
+} \
+\
+- (id)copyWithZone:(NSZone *)zone \
+{ \
+return _instace; \
+} \
+\
+- (oneway void)release { } \
+- (id)retain { return self; } \
+- (NSUInteger)retainCount { return 1;} \
+- (id)autorelease { return self;}
+
+#endif
+
